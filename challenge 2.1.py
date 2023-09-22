e5ad9c0019dbb0fcb139a4393ce932ad79158483
aluau02222128012 <144431087+aluau02222128012@users.noreply.github.com>
2023-09-22 16:08:44 +0530

    Add files via upload

---
+++ challenge 2.1.py
@@ -0,0 +1,20 @@
+
+class Player:
+  def play(self):
+    print ("The player is playing cricket.")
+    #Define the derived classs Batsman
+class Batsman (Player):
+  def play(self):
+    print("The batsman is batting. ")
+    #define the derived class  bowler
+class Bowler(Player):
+  def play(self):
+    print("the bowler is bowling.")
+    
+batsman=Batsman()
+bowler=Bowler()
+player=Player()
+
+player.play()
+batsman.play()
+bowler.play()
